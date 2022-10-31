b1e32fbc25e24e53178e0921f4cf8f1a9f4dcba6
Marianne Barton <mhood24@gmail.com>
2022-10-31 12:24:06 -0600

    Start Cypress testing

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,11 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Turing Cafe Reservations page flows', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
   })
+
+  it('Should be able to view the app page and see individual reservations', () => {
+    cy.intercept('GET', 'http://localhost:3001/api/v1/reservations')
+    .get(`.app-title`).contains('Turing Cafe Reservations')
+  })
+
 })
