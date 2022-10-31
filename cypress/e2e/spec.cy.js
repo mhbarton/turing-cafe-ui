8112c057b3838faab05bd5643bf040fc5fb42c6b
Marianne Barton <mhood24@gmail.com>
2022-10-31 13:24:44 -0600

    Add beginning cypress testing

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,8 +4,20 @@ describe('Turing Cafe Reservations page flows', () => {
   })
 
   it('Should be able to view the app page and see individual reservations', () => {
-    cy.intercept('GET', 'http://localhost:3001/api/v1/reservations')
+    cy.intercept('GET', 'http://localhost:3001/api/v1/reservations', {fixture: 'allReservations'}).as('reservations')
     .get(`.app-title`).contains('Turing Cafe Reservations')
+    .get(`.reservations-container > :nth-child(2)`).contains('Christie')
+    .get(`.reservations-container > :nth-child(2)`).contains(`12/29`)
+    .get(`.reservations-container > :nth-child(2)`).contains(`7:00 pm`)
+    .get(`.reservations-container > :nth-child(2)`).contains(`2`)
+ 
   })
 
+  it('Should be able to visit the page and let the user input information into the form', () => {
+    cy.get('form')
+      .get(`[placeholder="Name"]`)
+      .get(`[placeholder="Date (mm/dd)"]`)
+      .get(`[placeholder="Time"]`)
+      .get(`[placeholder="Number of guests"]`)
+  })
 })
